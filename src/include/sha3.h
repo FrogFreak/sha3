4fd220d5c79e53749f29cf86f07324ff635a3179
Yanis Mammar <yanis.mammar@epita.fr>
2022-11-07 03:47:44 +0100

    Add oneshot SHA3 and SHAKE functions

--- src/include/sha3.h
+++ src/include/sha3.h
@@ -25,31 +25,36 @@ typedef struct keccak1600_st {
 	SHA_LONG64 state[25];
 	char buf[168];
 	size_t bufsz;
-	unsigned finalized : 1;
 } SHA3_CTX;
 
 int SHA3_224_Init(SHA3_CTX *c);
 int SHA3_224_Update(SHA3_CTX *c, const void *data, size_t len);
 int SHA3_224_Final(unsigned char *md, SHA3_CTX *c);
+unsigned char *SHA3_224(unsigned char *md, const void *data, size_t len);
 
 int SHA3_256_Init(SHA3_CTX *c);
 int SHA3_256_Update(SHA3_CTX *c, const void *data, size_t len);
 int SHA3_256_Final(unsigned char *md, SHA3_CTX *c);
+unsigned char *SHA3_256(unsigned char *md, const void *data, size_t len);
 
 int SHA3_384_Init(SHA3_CTX *c);
 int SHA3_384_Update(SHA3_CTX *c, const void *data, size_t len);
 int SHA3_384_Final(unsigned char *md, SHA3_CTX *c);
+unsigned char *SHA3_384(unsigned char *md, const void *data, size_t len);
 
 int SHA3_512_Init(SHA3_CTX *c);
 int SHA3_512_Update(SHA3_CTX *c, const void *data, size_t len);
 int SHA3_512_Final(unsigned char *md, SHA3_CTX *c);
+unsigned char *SHA3_512(unsigned char *md, const void *data, size_t len);
 
 #ifdef SHA3_XOF
 int SHAKE128_Init(SHA3_CTX *c);
 int SHAKE128_Update(SHA3_CTX *c, const void *data, size_t len);
 int SHAKE128_Final(unsigned char *md, size_t mdlen, SHA3_CTX *c);
+unsigned char *SHAKE128(unsigned char *md, size_t mdlen, const void *data, size_t len);
 
 int SHAKE256_Init(SHA3_CTX *c);
 int SHAKE256_Update(SHA3_CTX *c, const void *data, size_t len);
 int SHAKE256_Final(unsigned char *md, size_t mdlen, SHA3_CTX *c);
+unsigned char *SHAKE256(unsigned char *md, size_t mdlen, const void *data, size_t len);
 #endif
